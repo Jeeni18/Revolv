7c08f862ad492497b53d477d73cc11abc05799ad
ishaa-bht <ishabhattarai21@gmail.com>
2025-02-10 16:33:41 +0545

    `Refactor Footer component CSS and JavaScript files`

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,40 +1,50 @@
-import "./Footer.css"
-import { Facebook, Twitter, Linkedin, Instagram } from "lucide-react"
+import "./Footer.css";
+import { Facebook, Twitter, Linkedin, Instagram } from "lucide-react";
 
 const Footer = () => {
   return (
-    <section id='contact'>
-    <footer className="footer">
-      <div className="footer-container">
-        {/* Copyright */}
-        <div className="footer-left">
-          <p>&copy; {new Date().getFullYear()} Resolv. All rights reserved.</p>
-        </div>
+    <section id="contact">
+      <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-content">
+            {/* Contact Info */}
+            <div className="footer-section">
+              <h4>Contact Us</h4>
+              <ul>
+                <li>contact@resolv.com</li>
+                <li>+1 (555) 123-4567</li>
+                <li>123 Business Ave, Suite 100</li>
+              </ul>
+            </div>
 
-        {/* Social Media Icons */}
-        <div className="footer-right">
-          <a href="#" className="social-icon">
-            <Facebook />
-            <span className="sr-only">Facebook</span>
-          </a>
-          <a href="#" className="social-icon">
-            <Twitter />
-            <span className="sr-only">Twitter</span>
-          </a>
-          <a href="#" className="social-icon">
-            <Linkedin />
-            <span className="sr-only">LinkedIn</span>
-          </a>
-          <a href="#" className="social-icon">
-            <Instagram />
-            <span className="sr-only">Instagram</span>
-          </a>
+            {/* Social Media */}
+            <div className="footer-section">
+              <h4>Connect With Us</h4>
+              <div className="social-icons">
+                <a href="#" className="social-icon" aria-label="Facebook">
+                  <Facebook size={20} />
+                </a>
+                <a href="#" className="social-icon" aria-label="Twitter">
+                  <Twitter size={20} />
+                </a>
+                <a href="#" className="social-icon" aria-label="LinkedIn">
+                  <Linkedin size={20} />
+                </a>
+                <a href="#" className="social-icon" aria-label="Instagram">
+                  <Instagram size={20} />
+                </a>
+              </div>
+            </div>
+          </div>
+
+          {/* Copyright Bar */}
+          <div className="footer-bottom">
+            <p>&copy; {new Date().getFullYear()} Resolv. All rights reserved.</p>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
     </section>
-  )
-}
+  );
+};
 
-export default Footer
-
+export default Footer;
